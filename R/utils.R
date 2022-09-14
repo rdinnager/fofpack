38dbd0fc5fd974c59f178d32c1fcb2d525639415
Russell Dinnage <rdinnage@fiu.edu>
2022-09-14 16:10:53 -0400

    added run_exam_1 function

---
+++ R/utils.R
@@ -0,0 +1,13 @@
+run_exam_1 <- function() {
+  download_file <- "https://raw.githubusercontent.com/rdinnager/fofpack/main/inst/tutorials/week_3/week_3.Rmd"
+  local_file <- file.path(system.file("tutorials", package = "fofpack"), "exam_1", "exam_1.Rmd")
+  good <- download.file(download_file, local_file)
+  if(good == 0) { message("Exam download successful!") } else {
+    local_file <- file.path(getwd(), "exam_1.Rmd")
+    good <- download.file(download_file, local_file)
+    if(good == 0) { message("Exam download successful!") } else { warning("Exam download failed") }
+  }
+  learnr::run_tutorial(local_file)
+}
+
+run_exam_1()
