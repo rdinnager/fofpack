9a6a5fbe440af26128797488b548062573aceba0
Russell Dinnage <rdinnage@fiu.edu>
2022-09-15 09:29:06 -0400

    fixed typo

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,5 @@
 run_exam_1 <- function() {
-  download_file <- "https://raw.githubusercontent.com/rdinnager/fofpack/main/inst/tutorials/week_3/week_3.Rmd"
+  download_file <- "https://raw.githubusercontent.com/rdinnager/fofpack/main/inst/tutorials/exam_1/exam_1.Rmd"
   local_file <- file.path(system.file("tutorials", package = "fofpack"), "exam_1", "exam_1.Rmd")
   good <- download.file(download_file, local_file)
   if(good == 0) { message("Exam download successful!") } else {
