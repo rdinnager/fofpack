dcb24dce56d7e1b82d2975c549af1ffa9c7f9571
rdinnager <r.dinnage@gmail.com>
2022-09-26 11:41:46 -0400

    added parks land cover data

--- data-raw/spatial.R
+++ data-raw/spatial.R
@@ -1,3 +1,5 @@
+
+library(gdalUtilities)
 library(sf)
 library(fofpack)
 library(tidyverse)
@@ -5,7 +7,7 @@ library(terra)
 
 data(IRC)
 
-parks <- st_read("parks_shape")
+parks <- st_read("extdata/parks_shape")
 
 
 IRC_sf <- IRC %>%
@@ -27,14 +29,52 @@ irc_spat <- IRC_sf %>%
               select(MANAME)) %>%
   st_as_sf()
 
-write_rds(irc_spat, "data/irc_spat.rds")
-rm(irc_spat)
-rm(parks)
-rm(IRC_sf)
-rm(IRC)
+# write_rds(irc_spat, "data/irc_spat.rds")
+# rm(irc_spat)
+# rm(parks)
+# rm(IRC_sf)
+# rm(IRC)
 
 ######### land cover ##############
 
 #CLC <- rast("data/CLC_raster/CLC_v3_5_Raster.gdb")
 CLC <- st_read("data/CLC_v3_5.gdb")
 
+ensure_multipolygons <- function(X) {
+    tmp1 <- tempfile(fileext = ".gpkg")
+    tmp2 <- tempfile(fileext = ".gpkg")
+    st_write(X, tmp1)
+    ogr2ogr(tmp1, tmp2, f = "GPKG", nlt = "MULTIPOLYGON")
+    Y <- st_read(tmp2)
+    st_sf(st_drop_geometry(X), geom = st_geometry(Y))
+}
+
+CLC <- CLC %>%
+  ensure_multipolygons()
+write_sf(CLC, "extdata/CLC.geojson")
+
+
+irc_lc <- irc_spat %>%
+  st_transform(st_crs(CLC)) %>%
+  st_join(CLC %>%
+            select(NAME_STATE) %>%
+            mutate(id = 1:n()))
+
+CLC_overlapped <- CLC %>%
+  slice(unique(irc_lc$id)) %>%
+  select(NAME_STATE)
+
+irc_lc_CLC <- st_intersection(irc_spat, CLC_overlapped)
+
+write_rds(irc_lc_CLC, "data/intermed_result.rds")
+
+irc_lc_CLC <- irc_lc_CLC %>%
+  mutate(area = st_area(geometry))
+
+irc_final <- irc_lc_CLC %>%
+  as_tibble() %>%
+  select(-geometry) %>%
+  group_by(area_name, NAME_STATE) %>%
+  summarise(area = sum(as.numeric(area))) %>%
+  group_by(area_name) %>%
+  mutate(prop = area / sum(area))
