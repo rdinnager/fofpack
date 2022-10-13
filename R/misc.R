b195ffcf8a0058fce9f779a1d02d72f99bd9cc8c
Russell Dinnage <rdinnage@fiu.edu>
2022-10-13 09:29:10 -0400

    Added exam 2

--- R/misc.R
+++ R/misc.R
@@ -3,5 +3,6 @@ make_data <- function(IRC, parks_LC, species) {
   IRC %>%
     filter(ScientificName == species) %>%
     left_join(parks_LC) %>%
-    drop_na()
+    drop_na() %>%
+    mutate(size = parse_number(size))
 }
